4067054a4274d5990bbb106dcb7baa2c2c7262e7
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-18 18:30:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,13 @@ resource "aws_elastic_beanstalk_application" "tftest" {
   name        = "tf-test-name"
   description = "tf-test-desc"
 }
+data "aws_elastic_beanstalk_solution_stack" "multi_docker" {
+  most_recent = true
 
+  name_regex = "^64bit Amazon Linux (.*) Multi-container Docker (.*)$"
+}
 resource "aws_elastic_beanstalk_environment" "tfenvtest" {
   name                = "tf-test-name"
   application         = aws_elastic_beanstalk_application.tftest.name
-  solution_stack_name = "64bit Amazon Linux 2015.03 v2.0.0 running Tomcat 8 Java 8"
+  solution_stack_name = aws_elastic_beanstalk_solution_stack.multi_docker.name_regex
 }
