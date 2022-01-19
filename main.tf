210a8095e078ef2b64271ec13bab64b72bbad448
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-19 10:40:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,8 @@ resource "aws_elastic_beanstalk_environment" "tfenvtest" {
   application         = aws_elastic_beanstalk_application.tftest.name
   solution_stack_name = "64bit Amazon Linux 2 v3.3.9 running Python 3.8"
 }
+ setting {
+      namespace = "aws:autoscaling:launchconfiguration"
+      name = "IamInstanceProfile"
+      value = "demo-Role"
+  }
